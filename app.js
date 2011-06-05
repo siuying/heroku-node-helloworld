242809ea051c3c9cc9e82c64bdcb79a6ac143ff0
Francis Chong <francis@ignition.hk>
2011-06-05 11:30:17 +0800

    use express logger

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var app = express.createServer();
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(req, res){
     res.send('Hello World');
